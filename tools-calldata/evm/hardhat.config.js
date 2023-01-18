d67e0007473d13235473c96366e96d1c337296d7
kyberliem <cyberliem.civil@gmail.com>
2023-01-18 14:18:00 +0700

    add hardhat config

--- tools-calldata/evm/hardhat.config.js
+++ tools-calldata/evm/hardhat.config.js
@@ -16,6 +16,9 @@ module.exports = {
                 version: '0.6.6',
             },
             {
+                version: '0.6.7',
+            },
+            {
                 version: '0.8.9',
             },
         ],
